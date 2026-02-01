f2e2199b14fb7661ae9a6cf50535fb48bfd2262d
Kennet Rodriguez <kennet_rl@hotmail.com>
2026-02-01 15:33:20 -0500

    fix: Se añadió temporalmente el url localhost por cords

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "http://localhost:8080"  # Agregado para permitir el frontend en el puerto 8080
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
