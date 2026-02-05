20a22c7fff9f972f8907f091dc46b739a536407f
Kennet Rodriguez <kennet_rl@hotmail.com>
2026-02-05 15:11:57 -0500

    feat: Se añadió la ip del vps en cords

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
+        "http://217.216.89.228:8080",
         "http://localhost:8080"  # Agregado para permitir el frontend en el puerto 8080
     ],
     allow_credentials=True,
